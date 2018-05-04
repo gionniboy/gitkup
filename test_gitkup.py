2678d9a06186a550b61a5041ddaa0a9542ea956f
gionni <giovbat@gmail.com>
2018-05-04 06:06:15 +0200

    removed useless tests

--- test_gitkup.py
+++ test_gitkup.py
@@ -15,31 +15,3 @@ def test_setup_logging_missing():
     with pytest.raises(SystemExit) as err:
         gitkup.setup_logging(filepath="logging.test")
     assert 'Create logging.json config file and restart.' in str(err.value)
-
-
-def test_validate_domain():
-    """ test domain validation ok"""
-    domain = 'python.org'
-    assert gitkup.validate_domain(domain) is True
-
-
-def test_validate_domain_exit():
-    """ test domain validation exit """
-    domain = 'python.org@it'
-    with pytest.raises(SystemExit) as err:
-        gitkup.validate_domain(domain)
-    assert 'Invalid domain specified.' in str(err.value)
-
-
-def test_validate_email():
-    """ test domain validation ok"""
-    email = 'test@python.org'
-    assert gitkup.validate_email(email) is True
-
-
-def test_validate_mail_exit():
-    """ test domain validation exit """
-    email = 'python.org@it'
-    with pytest.raises(SystemExit) as err:
-        gitkup.validate_email(email)
-    assert 'Invalid email specified.' in str(err.value)
